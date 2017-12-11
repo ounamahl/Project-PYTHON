90b6ebec3bdccb83836d5ed7cab3727cdad8f801
mahlapakk <34231777+mahlapakk@users.noreply.github.com>
2017-12-11 10:54:15 +0200

    Add files via upload

---
+++ atsialko.py
@@ -0,0 +1,9 @@
+#Programm, mis küsib kasutajalt alkohoolse joogi hinda Eestis (vastavalt, siis erinevate aktsiisidega lahjem alko, kangem alko) ja väljastab kasutajale selle joogi umbkaudse hinna Läti riigis ja vahe/võidu.
+#Õlle Eestis (hektoliitrile) 15.52 eur
+#Õlle Lätis (hektoliitrile) 8.2 eur
+hindeestis = float(input(" Mis on joogi hind Eestis?"))
+jook = str("Mis jooki ostate?")
+if jook == ("õlu"):
+    hindeestis
+    
+
