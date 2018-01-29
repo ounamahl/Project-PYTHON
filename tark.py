c67f87c33fc13cfaa8880b428470f6cc0b0d5393
mahlapakk <34231777+mahlapakk@users.noreply.github.com>
2018-01-29 10:59:47 +0200

    Add files via upload

---
+++ tark.py
@@ -0,0 +1,9 @@
+#Let it begin
+import webbrowser 
+üks = "tamme.ope.ee"
+kaks = "www.youtube.com"
+kolm = "www.facebook.com"
+neli = "www.auto24.ee"
+
+mislehte = input("Vali valikust üks leht: ")
+webbrowser.open_new(mislehte)
