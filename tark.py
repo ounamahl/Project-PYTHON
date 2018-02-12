d834806dafcbffea1dab10ea417c9cdb676c845c
mahlapakk <34231777+mahlapakk@users.noreply.github.com>
2018-02-12 10:46:55 +0200

    Update tark.py

--- tark.py
+++ tark.py
@@ -1,9 +1,23 @@
 #Let it begin
-import webbrowser 
+import webbrowser
+print("Veebilehed: 1 - Tamme stuudium; 2 - YouTube; 3 - Facebook; 4 - auto24")
+mislehte = input("Mis veebilehele minna soovid? ")
+
 üks = "tamme.ope.ee"
 kaks = "www.youtube.com"
 kolm = "www.facebook.com"
 neli = "www.auto24.ee"
 
-mislehte = input("Vali valikust üks leht: ")
-webbrowser.open_new(mislehte)
+if mislehte == '1':
+    webbrowser.open_new(üks)
+if mislehte == '2':
+    webbrowser.open_new(kaks)
+if mislehte == '3':
+    webbrowser.open_new(kolm)
+if mislehte == '4':
+    webbrowser.open_new(neli)
+
+
+
+
+
